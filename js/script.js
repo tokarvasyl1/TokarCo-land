05f0d8c47300363e8d8909b6ceac82146d61413e
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-12-28 15:22:08 +0200

    тест

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(function () {
+    $('.burger__menu').click(function () {
+        $('.menu__body,.burger__menu').toggleClass('opened')
+        $('body').toggleClass('lock')
+    });
+});
+
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+    document.addEventListener('click', documentActions);
+}
+
+function documentActions(e) {
+    const targetElement = e.target;
+
+    //scroll to
+
+
+    if (targetElement.hasAttribute('data-goto')) {
+        const gotoElement = document.querySelector(`${targetElement.dataset.goto}`);
+        const headerHeight = document.querySelector(`.header`).offsetHeight;
+
+        if (gotoElement) {
+            window.scrollTo({
+                top: gotoElement.offsetTop - headerHeight,
+                behavior: "smooth"
+            });
+        }
+        e.preventDefault();
+    }
+
+    //works filter
+    if (targetElement.classList.contains('goods__item__type') && !targetElement.classList.contains('active')){
+        const activeElement = document.querySelector(`.goods__item__type.active`);
+        const elements = document.querySelectorAll(`.goods__items-item`);
+        const elementType = targetElement.dataset.workType;
+
+        activeElement.classList.remove('active');
+        targetElement.classList.add('active');
+
+        elements.forEach(element => {
+            !elementType || element.dataset.workType === elementType ?
+                element.hidden = false : element.hidden = true;
+        })
+    }
+}
+
+
+
