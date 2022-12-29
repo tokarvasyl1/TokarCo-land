a67423a7aea43ffd431765e19d8332f993d18a9a
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-12-30 00:09:59 +0200

    full sections

--- js/script.js
+++ js/script.js
@@ -49,3 +49,27 @@ function documentActions(e) {
 
 
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:false
+        }
+    }
+})
+
+
+
+
