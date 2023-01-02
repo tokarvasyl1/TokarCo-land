4fcd38fe9fd6126cca832739f9e417f151963443
tokarvasyl1 <tokarvasyl1@gmail.com>
2023-01-02 17:26:43 +0200

    full sections

--- js/script.js
+++ js/script.js
@@ -59,18 +59,89 @@ $('.owl-carousel').owlCarousel({
 })
 
 $(function () {
-    $('.button__project').click(function () {
-        $('.popup__bg').addClass('opened')
+    $('.o1').click(function () {
+        $('.popup__bg-o1').addClass('opened')
         $('body').addClass('lock')
     });
     $('.close__popup').click(function () {
-        $('.popup__bg').removeClass('opened')
+        $('.popup__bg-o1').removeClass('opened')
         $('body').removeClass('lock')
     });
 });
 
+$(function () {
+    $('.o3').click(function () {
+        $('.popup__bg-o3').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o3').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
 
+$(function () {
+    $('.o5').click(function () {
+        $('.popup__bg-o5').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o5').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
 
+$(function () {
+    $('.o6').click(function () {
+        $('.popup__bg-o6').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o6').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
 
+$(function () {
+    $('.o7').click(function () {
+        $('.popup__bg-o7').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o7').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
 
+$(function () {
+    $('.o8').click(function () {
+        $('.popup__bg-o8').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o8').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
 
+$(function () {
+    $('.o9').click(function () {
+        $('.popup__bg-o9').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o9').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
+
+$(function () {
+    $('.o10').click(function () {
+        $('.popup__bg-o10').addClass('opened')
+        $('body').addClass('lock')
+    });
+    $('.close__popup').click(function () {
+        $('.popup__bg-o10').removeClass('opened')
+        $('body').removeClass('lock')
+    });
+});
